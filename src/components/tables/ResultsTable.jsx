38c3acca143bb0b22694471a49a0b0c597589601
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-02-15 22:41:43 +0300

    Add templates for pages

---
+++ src/components/tables/ResultsTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ResultsTable() {
+  return <div>ResultsTable</div>;
+}
+
+export default ResultsTable;
---
+++ src/components/tables/ScheduleTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ScheduleTable() {
+  return <div>ScheduleTable</div>;
+}
+
+export default ScheduleTable;
