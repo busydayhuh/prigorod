c72f63fc9a2b5b4a74b1c447736308d6f563e6de
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-02-28 12:03:17 +0300

    Add filter by express type

--- src/components/results/ResultsTable.jsx
+++ src/components/results/ResultsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -14,61 +14,82 @@ import useApi from "@/lib/api";
 
 function ResultsTable() {
   const [searchParams] = useSearchParams();
-  const [isDepartedOpen, setIsDepartedOpen] = useState(false);
-
   const { data, isLoading, isError } = useApi("search", searchParams);
+
+  const [isDepartedOpen, setIsDepartedOpen] = useState(false);
+  const [expressOnly, setExpressOnly] = useState(false);
+
+  const filterResults = (results) => {
+    if (expressOnly) {
+      const expressOnlyResults = results.filter((result) => {
+        return result.thread.express_type;
+      });
+
+      return expressOnlyResults;
+    }
+
+    return results;
+  };
 
   return isLoading ? (
     <p>Loading...</p>
   ) : (
-    <div className="w-[min(56rem,96%)] mx-auto rounded-md border shadow-sm">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Отправление</TableHead>
-            <TableHead>Время в пути</TableHead>
-            <TableHead>Прибытие</TableHead>
-            <TableHead>Поезд</TableHead>
-            <TableHead>Цена</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {isDepartedOpen ? (
-            data.departed.map((segment) => {
+    <>
+      <div className="w-[min(56rem,96%)] mx-auto rounded-md border shadow-sm">
+        <Button
+          className="max-w-[150px]"
+          onClick={() => setExpressOnly((prev) => !prev)}
+        >
+          Только экспрессы
+        </Button>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Отправление</TableHead>
+              <TableHead>Время в пути</TableHead>
+              <TableHead>Прибытие</TableHead>
+              <TableHead>Поезд</TableHead>
+              <TableHead>Цена</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {isDepartedOpen ? (
+              filterResults(data.departed).map((segment) => {
+                return (
+                  <Row
+                    key={`${segment.departure}${segment.thread.number}`}
+                    departed={true}
+                    {...segment}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell className="w-[100px]">
+                  <Button
+                    variant="secondary"
+                    onClick={() => {
+                      setIsDepartedOpen(true);
+                    }}
+                  >
+                    Показать
+                  </Button>
+                </TableCell>
+              </TableRow>
+            )}
+
+            {filterResults(data.future).map((segment) => {
               return (
                 <Row
                   key={`${segment.departure}${segment.thread.number}`}
-                  departed={true}
                   {...segment}
                 />
               );
-            })
-          ) : (
-            <TableRow>
-              <TableCell className="w-[100px]">
-                <Button
-                  variant="secondary"
-                  onClick={() => {
-                    setIsDepartedOpen(true);
-                  }}
-                >
-                  Показать
-                </Button>
-              </TableCell>
-            </TableRow>
-          )}
-
-          {data.future.map((segment) => {
-            return (
-              <Row
-                key={`${segment.departure}${segment.thread.number}`}
-                {...segment}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>
-    </div>
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 }
 
