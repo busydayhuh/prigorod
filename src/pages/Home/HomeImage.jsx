4ab7036652b8be8abc6a02ef900e9349add0db21
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-05-12 19:55:12 +0300

    Fix alignment and positioning of images in HomeImage component

--- src/pages/Home/HomeImage.jsx
+++ src/pages/Home/HomeImage.jsx
@@ -6,14 +6,14 @@ function HomeImage() {
   return (
     <figure
       id="home-img"
-      className="flex items-center justify-start min-w-[320px] pointer-events-none md:pointer-events-auto"
+      className="flex items-center justify-center md:justify-start min-w-[320px] pointer-events-none md:pointer-events-auto"
     >
       <div className="relative max-w-[500px] sm:w-[100%] w-[90%] aspect-square rounded-[50%] bg-foreground">
         <img
           id="train"
           src={Train}
           alt="Поезд Ласточка"
-          className="max-w-[569px] sm:w-[140%] w-[95%] absolute left-[15%] top-[22%]"
+          className="max-w-[569px] sm:w-[140%] w-[95%] absolute left-[10%] md:left-[15%] top-[22%]"
         />
         <img
           id="tick"
@@ -22,7 +22,7 @@ function HomeImage() {
         />
         <img
           src={RoundCorner}
-          className="absolute left-[14%] md:left-[16%] top-[40%] md:top-[47%] sm:rotate-[10deg] rotate-[-20deg]"
+          className="absolute left-[10%] md:left-[16%] top-[40%] md:top-[47%] sm:rotate-[10deg] rotate-[-20deg]"
         />
       </div>
     </figure>
