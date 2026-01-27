9e83b9a13321a538fb7dcac53a760dce4f9558fe
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2026-01-27 17:49:59 +0300

    add new provider and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import { Outlet } from "react-router";
+import Hero from "./components/Hero";
 import { ScrollUpBtn } from "./components/ui";
 import FormContextProvider from "./store/form/FormContextProvider";
 import PrevSearchesProvider from "./store/form/PrevSearchesProvider";
-import { IsDesktopProvider } from "./store/WindowSizeContext";
+import { IsMobileProvider } from "./store/window-size/IsMobileProvider";
 
 function App() {
   return (
     <FormContextProvider>
       <PrevSearchesProvider>
-        <IsDesktopProvider>
+        <IsMobileProvider>
           <div className="relative">
-            <Header />
+            <Hero />
             <main className="mt-5 md:mt-10 overflow-hidden">
               <Outlet />
             </main>
             <Footer />
             <ScrollUpBtn />
           </div>
-        </IsDesktopProvider>
+        </IsMobileProvider>
       </PrevSearchesProvider>
     </FormContextProvider>
   );
